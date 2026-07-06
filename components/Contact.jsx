5436e0ca7069770a34d34a99b968340b032d4bbd
luffy809 <ns7745922@gmail.com>
2026-07-06 12:26:58 +0530

    gittt

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,4 +1,62 @@
+import { useState } from "react";
+
 const Contact = () => {
+
+const [formData, setFormData] = useState({
+  fullName : "",
+  message : "",
+  subject : "",
+  email : "",
+})
+
+const [loading, setLoading] = useState(false);
+const [message , setMessage] = useState("");
+
+
+const handleChanges = (e)=>{
+  const {name,value} = e.target;
+  setFormData({...formData,[name]:value})
+}
+const handleSubmit = async(e)=>{
+    e.preventDefault();
+
+    setLoading(true);
+    setMessage("");
+
+    try{
+      const response = await fetch("http://localhost:8000/api/create",{
+        method:"POST",
+        headers:{
+          "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+          ...formData
+        })
+      })
+
+      const data = await response.json();
+      if(!response.ok){
+        setMessage(data.message || "something went wrong!");
+        setLoading(false);
+        return;
+      }
+      setMessage("client created successfully!")
+      setFormData({
+        fullName:"",
+        message:"",
+        subject:"",
+        email:"",
+      })
+    }
+    catch{
+      setMessage("An error occurred while submitting the form.");
+      setLoading(false);
+    }
+    finally{
+      setLoading(false);
+    }
+    }
+
   return (
     <section className="min-h-screen bg-[#F8F5F2] px-8 py-20">
       <div className="max-w-7xl mx-auto flex flex-col lg:flex-row gap-16">
@@ -21,14 +79,14 @@ const Contact = () => {
               <p className="text-sm uppercase tracking-widest text-gray-500">
                 Email
               </p>
-              <p className="text-lg">schhavi625@gmail.com</p>
+              <p className="text-lg">schhavi@gmail.com</p>
             </div>
 
             <div>
               <p className="text-sm uppercase tracking-widest text-gray-500">
                 Phone
               </p>
-              <p className="text-lg">+91 9876434396</p>
+              <p className="text-lg">+91 98********</p>
             </div>
 
             <div>
@@ -43,15 +101,21 @@ const Contact = () => {
 
         <div className="lg:w-1/2 bg-white rounded-3xl shadow-xl p-10">
 
-          <form className="space-y-6">
+          <form onSubmit={handleSubmit} className="space-y-6">
 
+               {message && (
+          <div className="bg-green-100 text-green-800 px-4 py-2 rounded mb-4">
+            {message}
+          </div>
+        )}
+        
             <div>
               <label className="block mb-2 text-sm text-gray-600">
                 Full Name
               </label>
 
               <input
-                type="text"
+                type="text" name="fullName" onChange={handleChanges} required value={formData.fullName}
                 placeholder="John Doe"
                 className="w-full border border-gray-300 rounded-xl px-5 py-4 outline-none focus:border-[#B58863]"
               />
@@ -63,7 +127,7 @@ const Contact = () => {
               </label>
 
               <input
-                type="email"
+                type="email" name="email" required onChange={handleChanges} value={formData.email}
                 placeholder="john@example.com"
                 className="w-full border border-gray-300 rounded-xl px-5 py-4 outline-none focus:border-[#B58863]"
               />
@@ -75,7 +139,7 @@ const Contact = () => {
               </label>
 
               <input
-                type="text"
+                type="text" name="subject" onChange={handleChanges} required value={formData.subject}
                 placeholder="Project Inquiry"
                 className="w-full border border-gray-300 rounded-xl px-5 py-4 outline-none focus:border-[#B58863]"
               />
@@ -87,16 +151,18 @@ const Contact = () => {
               </label>
 
               <textarea
-                rows="6"
+                rows="6" onChange={handleChanges} required value={formData.message} name="message"
                 placeholder="Write your message..."
                 className="w-full border border-gray-300 rounded-xl px-5 py-4 outline-none focus:border-[#B58863]"
               ></textarea>
             </div>
 
             <button
+              type="submit"
               className="bg-[black] text-white px-8 py-4 rounded-xl hover:translate-y-2 transition duration-300 hover:bg-[#52525B]"
             >
-              Send Message
+               {loading ? "Sending..." : "Send Message"}
+
             </button>
 
           </form>
